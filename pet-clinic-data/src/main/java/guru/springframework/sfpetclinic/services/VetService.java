ad510049eefa01ca038f8e93f32333f5502673fe
henriquewallace <henrique.wallace@hotmail.com>
2023-07-20 23:20:33 -0300

    create services interfaces. closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfpetclinic.services;
+
+import guru.springframework.sfpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
