fccac666deb8f6a7da5c1d42b440e348302a618a
henriquewallace <henrique.wallace@hotmail.com>
2023-08-09 20:52:05 -0300

    create pet type service map. closes #28

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfpetclinic.services;
+
+import guru.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
