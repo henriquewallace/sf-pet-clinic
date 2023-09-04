b34df74469d820d204a588077ad217fafeabea9f
henriquewallace <henrique.wallace@hotmail.com>
2023-09-04 00:13:44 -0300

    set up owner controller test

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.sfpetclinic.controllers;
+
+import guru.springframework.sfpetclinic.model.Owner;
+import guru.springframework.sfpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController ownerController;
+
+    Set<Owner> owners;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @Test
+    void ownersList() {
+    }
+
+    @Test
+    void findOwners() {
+    }
+}
